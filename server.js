37ffff861a6d593d0d1f899fad39fda5f6f04839
Bonganibg <bongani.gumbo@yahoo.com>
2022-03-16 16:33:46 +0200

    deploy 2.0

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/price-checker'));
+app.use(express.static('./dist/price-checker'));
 
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname+'/dist/price-checker/index.html'));
+  res.sendFile('index.html', {root: 'dist/price-checker/'})
 });
 
 const port = (process.env.PORT || 8080);
 
+console.log("server is running")
 app.set("port", port);
 app.listen(port);
