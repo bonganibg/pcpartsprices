60438c465f9dc310df7232e7431fbe9d90612ade
Bonganibg <bongani.gumbo@yahoo.com>
2022-03-16 13:18:02 +0200

    server changes

--- server.js
+++ server.js
@@ -9,4 +9,7 @@ app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname+'/dist/price-checker/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+const port = (process.env.PORT || 8080);
+
+app.set("port", port);
+app.listen(port);
